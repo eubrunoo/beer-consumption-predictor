603fb3562945c2a6825089c07bc8eb269ea24093
Bruno Moreira <moreirabcf04@gmail.com>
2024-09-13 23:15:54 -0300

    pearson correlation Matrix

--- main.R
+++ main.R
@@ -1,3 +1,6 @@
+library('tidyverse')
+library('tidyquant')
+
 beer_data <- read.csv("beer_consumption.csv")
 names(beer_data) <- c("Date",
                       "Average_Temperature",
@@ -8,3 +11,19 @@ names(beer_data) <- c("Date",
                       "Beer_Consumption_Liters")
 
 dim(beer_data)
+summary(beer_data)
+
+corr_columns <- c("Average_Temperature", "Precipitation", "Beer_Consumption_Liters")
+selected_data <- beer_data[corr_columns]
+str(selected_data)
+
+selected_data$Average_Temperature <- gsub(",", ".", selected_data$Average_Temperature)
+selected_data$Precipitation <- gsub(",", ".", selected_data$Precipitation)
+
+selected_data$Average_Temperature <- as.numeric(selected_data$Average_Temperature)
+selected_data$Precipitation <- as.numeric(selected_data$Precipitation)
+
+invalid_entries <- grepl("invalid number", selected_data$Beer_Consumption_Liters, ignore.case = TRUE)
+selected_data <- selected_data[complete.cases(selected_data), ]
+
+M <- cor(selected_data)
